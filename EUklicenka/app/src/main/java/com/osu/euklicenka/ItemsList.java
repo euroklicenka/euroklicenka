f756d8fe7441df9f4431197355341678194112aa
WaluigiNumber8 <p22157@student.osu.cz>
2022-10-17 18:09:45 +0200

    Test 2 nahrání projektu EUklicenka

---
+++ EUklicenka/app/src/main/java/com/osu/euklicenka/ItemsList.java
@@ -0,0 +1,6 @@
+package com.osu.euklicenka;
+
+import android.app.Activity;
+
+public class ItemsList extends Activity {
+}
---
+++ EUklicenka/app/src/main/java/com/osu/euklicenka/Model.kt
@@ -0,0 +1,3 @@
+package com.osu.euklicenka
+
+class Model(val title:String, val desc:String, val photo:Int )
