ea4e5d09cb7bd2168ec2231bfed0fffc4a74f548
ondrej66 <78900959+ondrej66@users.noreply.github.com>
2023-02-14 22:12:19 +0100

    csvreader

---
+++ euk2_project/lib/csvreader.dart
@@ -0,0 +1,74 @@
+import 'package:flutter/material.dart';
+import 'package:csv/csv.dart';
+
+void main() {
+  runApp(
+    const MaterialApp(
+      home: Homepage(),
+    ),
+  );
+}
+
+class Homepage extends StatefulWidget {
+  const Homepage({super.key});
+
+  @override
+  State<Homepage> createState() => _HomepageState();
+}
+
+class _HomepageState extends State<Homepage> {
+  List<List<dynamic>>? csvData;
+
+  Future<List<List<dynamic>>> processCsv() async {
+    var result = await DefaultAssetBundle.of(context).loadString(
+      "assets/data/test.csv",
+    );
+    return const CsvToListConverter().convert(result, eol: "\n");
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text("Csv reader"),
+      ),
+      body: SingleChildScrollView(
+        scrollDirection: Axis.horizontal,
+        child: csvData == null
+            ? const CircularProgressIndicator()
+            : DataTable(
+          columns: csvData![0]
+              .map(
+                (item) => DataColumn(
+              label: Text(
+                item.toString(),
+              ),
+            ),
+          )
+              .toList(),
+          rows: csvData!
+              .map(
+                (csvrow) => DataRow(
+              cells: csvrow
+                  .map(
+                    (csvItem) => DataCell(
+                  Text(
+                    csvItem.toString(),
+                  ),
+                ),
+              )
+                  .toList(),
+            ),
+          )
+              .toList(),
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () async {
+          csvData = await processCsv();
+          setState(() {});
+        },
+      ),
+    );
+  }
+}
