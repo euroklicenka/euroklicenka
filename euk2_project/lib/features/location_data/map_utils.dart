5addd880f9433b461f6bd96f099f3dcd82661f57
WaluigiNumber8 <p22157@student.osu.cz>
2023-02-27 17:41:17 +0100

    EUR2-99 Removed EUK_Marker
    * Removed the EUKMarker class as it was not used by the app.

--- euk2_project/lib/features/location_data/map_utils.dart
+++ euk2_project/lib/features/location_data/map_utils.dart
@@ -1,23 +1,16 @@
 import 'package:custom_info_window/custom_info_window.dart';
+import 'package:euk2_project/features/icon_management/icon_manager.dart';
 import 'package:euk2_project/features/location_data/data/euk_location_data.dart';
-import 'package:euk2_project/features/location_data/data/euk_marker.dart';
 import 'package:euk2_project/features/popup_window/popup_window.dart';
 import 'package:flutter/material.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 
 /// Converts [EUKLocationData] into Google Maps [Marker] data.
-Future<Set<Marker>> convertToMarkers(List<EUKLocationData> data,
-    CustomInfoWindowController windowController) async {
+Future<Set<Marker>> convertToMarkers(List<EUKLocationData> dataList, CustomInfoWindowController windowController) async {
   final Set<Marker> markers = {};
 
-  for (final EUKLocationData location in data) {
-    markers.add(
-      await EUKMarker.create(
-        id: markers.length.toString(),
-        data: location,
-        windowController: windowController,
-      ),
-    );
+  for (final EUKLocationData data in dataList) {
+    markers.add(await _buildMarker(data, windowController));
   }
 
   return markers;
@@ -34,5 +27,16 @@ Widget buildPopUpWindow(EUKLocationData data) {
     imageURL:
         //TODO Replace or Remove once we know how we are going to put in pictures.
         'http://polar.cz/data/gallery/modules/polar/news/articles/videos/20200319151335_301/715x402.jpg?ver=20200319151525',
+  );
+}
+
+///Builds a new EUK Location map marker.
+Future<Marker> _buildMarker(EUKLocationData data, CustomInfoWindowController windowController) async {
+  final BitmapDescriptor icon = await getMarkerIconByType(data.type);
+  return Marker(
+    markerId: MarkerId(data.id),
+    icon: icon,
+    position: LatLng(data.lat, data.long),
+    onTap: () => windowController.addInfoWindow!(buildPopUpWindow(data), LatLng(data.lat, data.long)),
   );
 }
