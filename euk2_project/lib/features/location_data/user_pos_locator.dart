e3b846447ac4c35c9ba816c8a80cb57bc5552c3b
WaluigiNumber8 <p22157@student.osu.cz>
2023-03-22 17:31:50 +0100

    EUR2-123 Fixed app crashing when denied permissions.
    * The app no longer crashes when permissions are denied on first startup.

--- euk2_project/lib/features/location_data/user_pos_locator.dart
+++ euk2_project/lib/features/location_data/user_pos_locator.dart
@@ -1,16 +1,17 @@
 
 import 'package:euk2_project/features/snack_bars/snack_bar_management.dart';
+import 'package:flutter/services.dart';
 import 'package:google_maps_flutter/google_maps_flutter.dart';
 import 'package:location/location.dart';
 
 ///Tracks users current position.
 class UserPositionLocator {
   final Location _loc = Location();
-  late bool _serviceEnabled;
-  late PermissionStatus _permissionGranted;
-  late LocationData _locData;
   final double zoomAmount = 15;
   final LatLng _defaultPos = const LatLng(50.073658, 14.418540);
+
+  late bool _serviceEnabled;
+  LocationData? _locData;
   LatLng _currentPosition = const LatLng(0, 0);
 
   ///Initializes the location service.
@@ -20,21 +21,16 @@ class UserPositionLocator {
       _serviceEnabled = await _loc.requestService();
       if (!_serviceEnabled) {
         showSnackBar(message: 'Musíš aktivovat sledování polohy, aby aplikace mohla správně fungovat.');
-        return;
       }
     }
 
-    _permissionGranted = await _loc.hasPermission();
-    if (_permissionGranted == PermissionStatus.denied) {
-      _permissionGranted = await _loc.requestPermission();
-      if (_permissionGranted != PermissionStatus.granted) {
-        showSnackBar(message: 'Musíš povolit aplikaci přístup k poloze, aby mohla správně fungovat.');
-        return;
-      }
+    try {
+      _locData = await _loc.getLocation();
+    } on PlatformException {
+      showSnackBar(message: 'Musíš povolit aplikaci přístup k poloze, aby mohla správně fungovat.');
     }
 
-    _locData = await _loc.getLocation();
-    _currentPosition = LatLng(_locData.latitude ?? _defaultPos.latitude, _locData.longitude ?? _defaultPos.longitude);
+    _currentPosition = LatLng(_locData?.latitude ?? _defaultPos.latitude, _locData?.longitude ?? _defaultPos.longitude);
   }
 
   ///Updates the current position of the device.
