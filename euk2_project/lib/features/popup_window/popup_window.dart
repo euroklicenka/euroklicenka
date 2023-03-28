31d1f695132b743c8c665c7eb7cbeb1a7eb5a231
WaluigiNumber8 <p22157@student.osu.cz>
2023-03-28 13:38:48 +0200

    EUR2-86 PopupWindow is now a proper widget
    * Popup window is now a stateless widget instead of a function returning a widget.

--- euk2_project/lib/features/popup_window/popup_window.dart
+++ euk2_project/lib/features/popup_window/popup_window.dart
@@ -1,121 +1,135 @@
 import 'package:flutter/material.dart';
 import 'package:maps_launcher/maps_launcher.dart';
 
-Widget EUKPopupWindow({
-  required String address,
-  required String region,
-  required String city,
-  required String ZIP,
-  required String info,
-  required double lat,
-  required double long,
-}) {
-  const double headerSize = 18;
-  const double textSize = 16;
-  const double elementSpace = 4;
-  final Color? iconColor = Colors.amber[900];
+class EUKPopupWindow extends StatelessWidget {
+  late String _address, _region, _city, _ZIP, _info;
+  late double _lat, _long;
 
-  return Container(
-    width: 300,
-    height: 200,
-    decoration: BoxDecoration(
-      color: Colors.white,
-      border: Border.all(color: Colors.grey),
-      borderRadius: BorderRadius.circular(10.0),
-    ),
-    child: Padding(
-      padding: const EdgeInsets.only(left:12.0, right: 12.0, top: 12.0, bottom: 6.0),
-      child: Column(
-        children:[
-          Text(
-            address,
-            maxLines: 3,
-            style: const TextStyle(fontSize: headerSize, fontWeight: FontWeight.bold),
-          ),
-          const Divider(),
-          Expanded(
-            child: Scrollbar(
-              thumbVisibility: true,
-              child: Padding(
-                padding: const EdgeInsets.only(right: 6),
-                child: SingleChildScrollView(
-                  child: Column(
-                    crossAxisAlignment: CrossAxisAlignment.start,
-                    children: [
-                      Row(
-                        children: [
-                          Tooltip(
-                            message: 'Město',
-                            triggerMode: TooltipTriggerMode.tap,
-                            child: Icon(Icons.location_city_outlined, color: iconColor,),),
-                          const SizedBox(width: 8,),
-                          Flexible(
-                            child: Text(
-                              '$city, $ZIP',
-                              maxLines: 2,
-                              style: const TextStyle(fontSize: textSize),
+  EUKPopupWindow({super.key, required String address, required String region,
+      required String city, required String ZIP, required String info,
+      required double lat, required double long}) {
+    _address = address;
+    _region = region;
+    _city = city;
+    _ZIP = ZIP;
+    _info = info;
+    _lat = lat;
+    _long = long;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    const double headerSize = 18;
+    const double textSize = 16;
+    const double elementSpace = 4;
+    final Color? iconColor = Colors.amber[900];
+    return Container(
+      width: 300,
+      height: 200,
+      decoration: BoxDecoration(
+        color: Colors.white,
+        border: Border.all(color: Colors.grey),
+        borderRadius: BorderRadius.circular(10.0),
+      ),
+      child: Padding(
+        padding: const EdgeInsets.only(left: 12.0, right: 12.0, top: 12.0, bottom: 6.0),
+        child: Column(
+          children: [
+            Text(
+              _address,
+              maxLines: 3,
+              style: const TextStyle(fontSize: headerSize, fontWeight: FontWeight.bold),
+            ),
+            const Divider(),
+            Expanded(
+              child: Scrollbar(
+                thumbVisibility: true,
+                child: Padding(
+                  padding: const EdgeInsets.only(right: 6),
+                  child: SingleChildScrollView(
+                    child: Column(
+                      crossAxisAlignment: CrossAxisAlignment.start,
+                      children: [
+                        Row(
+                          children: [
+                            Tooltip(
+                              message: 'Město',
+                              triggerMode: TooltipTriggerMode.tap,
+                              child: Icon(
+                                Icons.location_city_outlined,
+                                color: iconColor,
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: elementSpace,),
-                      Row(
-                        children: [
-                          Tooltip(
-                            message: 'Kraj',
-                            triggerMode: TooltipTriggerMode.tap,
-                            child: Icon(Icons.map, color: iconColor,),),
-                          const SizedBox(width: 8,),
-                          Flexible(
-                            child: Text(
-                              region,
-                              maxLines: 2,
-                              style: const TextStyle(fontSize: textSize),
+                            const SizedBox(width: 8),
+                            Flexible(
+                              child: Text(
+                                '$_city, $_ZIP',
+                                maxLines: 2,
+                                style: const TextStyle(fontSize: textSize),
+                              ),
                             ),
-                          ),
-                        ],
-                      ),
-                      const SizedBox(height: elementSpace,),
-                      Row(
-                        children: [
-                          Tooltip(
-                            message: 'Info',
-                            triggerMode: TooltipTriggerMode.tap,
-                            child: Icon(Icons.info_outline, color: iconColor,),),
-                          const SizedBox(width: 8,),
-                          Flexible(
-                            child: Text(
-                              info,
-                              style: const TextStyle(fontSize: textSize),
-                              maxLines: 3,
+                          ],
+                        ),
+                        const SizedBox(height: elementSpace),
+                        Row(
+                          children: [
+                            Tooltip(
+                              message: 'Kraj',
+                              triggerMode: TooltipTriggerMode.tap,
+                              child: Icon(Icons.map, color: iconColor,),
                             ),
-                          ),
-                        ],
-                      ),
-                    ],
+                            const SizedBox(width: 8),
+                            Flexible(
+                              child: Text(
+                                _region,
+                                maxLines: 2,
+                                style: const TextStyle(fontSize: textSize),
+                              ),
+                            ),
+                          ],
+                        ),
+                        const SizedBox(height: elementSpace),
+                        Row(
+                          children: [
+                            Tooltip(
+                              message: 'Info',
+                              triggerMode: TooltipTriggerMode.tap,
+                              child: Icon(Icons.info_outline, color: iconColor,),
+                            ),
+                            const SizedBox(width: 8),
+                            Flexible(
+                              child: Text(
+                                _info,
+                                style: const TextStyle(fontSize: textSize),
+                                maxLines: 3,
+                              ),
+                            ),
+                          ],
+                        ),
+                      ],
+                    ),
                   ),
                 ),
               ),
             ),
-          ),
-          const Divider(height: elementSpace,),
-          Row(
-            mainAxisAlignment: MainAxisAlignment.end,
-            children: [
-              ElevatedButton(
-                onPressed: () {
-                  MapsLauncher.launchCoordinates(lat, long, address);
-                },
-                child: const Text(
-                  'Navigovat',
-                  style: TextStyle(fontSize: 16),
-                ),
-              )
-            ],
-          ),
-        ],
+            const Divider(
+              height: elementSpace,
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                ElevatedButton(
+                  onPressed: () {
+                    MapsLauncher.launchCoordinates(_lat, _long, _address);
+                  },
+                  child: const Text('Navigovat', style: TextStyle(fontSize: 16),
+                  ),
+                )
+              ],
+            ),
+          ],
+        ),
       ),
-
-    ),
-  );
+    );
+  }
 }
