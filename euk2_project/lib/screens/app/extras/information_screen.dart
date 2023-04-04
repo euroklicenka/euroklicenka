ece854e807d1080d6a888e6a75d874ef4e8f8a74
WaluigiNumber8 <p22157@student.osu.cz>
2023-04-04 18:21:40 +0200

    EUR2-87 Designed Info Screen base
    * Designed the skeleton of the info screen.
    * Added version label and image to info screen.
    * Added information about the company behind the app.
    * Created a new widget: InformationTile which shows text next to a logo.

--- euk2_project/lib/screens/app/extras/information_screen.dart
+++ euk2_project/lib/screens/app/extras/information_screen.dart
@@ -1,3 +1,4 @@
+import 'package:euk2_project/widgets/information_tile.dart';
 import 'package:flutter/material.dart';
 
 ///The information screen, that shows general info about the app as well as
@@ -7,12 +8,43 @@ class InformationScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    final screenSize = MediaQuery.of(context).size;
     return Scaffold(
       appBar: AppBar(
         title: const Text('Informace'),
       ),
-      body: const Center(
-        child: Text('Future Information screen here!!!'),
+      body: Center(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 24),
+          child: Column(
+            children: [
+              const SizedBox(height: 64),
+              Image.asset('assets/images/logo_key.png',
+                  width: screenSize.width * 0.25),
+              const SizedBox(height: 12),
+              const Text(
+                'EuroKlíčenka 1.0.0',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                  fontSize: 24,
+                ),
+              ),
+              const Expanded(
+                child: Align(
+                  alignment: Alignment.bottomCenter,
+                  child: Divider(),
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomCenter,
+                child: InfoTile(
+                    screenSize: screenSize,
+                    title: 'EuroKlíčenka je majetkem přírodověděcké fakulty Ostravské univerzity v Ostravě, Česká Republika.',
+                    imageFilePath: 'assets/images/prf_logo.png'),
+              ),
+            ],
+          ),
+        ),
       ),
     );
   }
