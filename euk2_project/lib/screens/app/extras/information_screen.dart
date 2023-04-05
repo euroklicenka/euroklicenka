5a8e6173c8efc358a91406dd5b6216d4a8e7a8b8
WaluigiNumber8 <p22157@student.osu.cz>
2023-04-05 17:52:21 +0200

    EUR2-87 Added All Stars info
    * There is a new card on the info screen, talking about teh developers of the app.
    * The info card list on the information screen is now scrollable.

--- euk2_project/lib/screens/app/extras/information_screen.dart
+++ euk2_project/lib/screens/app/extras/information_screen.dart
@@ -19,7 +19,8 @@ class InformationScreen extends StatelessWidget {
           padding: const EdgeInsets.symmetric(vertical: 24, horizontal: 24),
           child: Column(
             children: [
-              const SizedBox(height: 64),
+
+              const SizedBox(height: 56),
               Image.asset('assets/images/logo_key.png',
                   width: screenSize.width * 0.25),
               const SizedBox(height: 12),
@@ -31,23 +32,35 @@ class InformationScreen extends StatelessWidget {
                 ),
               ),
               Expanded(
-                child: Column(
-                  mainAxisAlignment: MainAxisAlignment.end,
-                  children: [
-                    const Spacer(),
-                    const Divider(),
-                    InfoTile(
-                        screenSize: screenSize,
-                        title: 'EuroKlíčenka je majetkem přírodověděcké fakulty Ostravské univerzity v Ostravě, Česká Republika.',
-                        imageFilePath: 'assets/images/logo_prf.png',
+                child: Padding(
+                  padding: const EdgeInsets.only(top: 32),
+                  child: Scrollbar(
+                    thumbVisibility: true,
+                    child: SingleChildScrollView(
+                      reverse: true,
+                      child: Column(
+                        children: [
+                          InfoTile(
+                            screenSize: screenSize,
+                            title: 'Za vývojem nové podoby EuroKlíčenky stojí tým studentů OU, do kterého patří Jan Sonnek, Jan Kunetka, Ondřej Sládek a Ondřej Zeman.',
+                            imageFilePath: 'assets/images/logo_ou.png',
+                          ),
+                          const Divider(),
+                          InfoTile(
+                            screenSize: screenSize,
+                            title: 'EuroKlíčenka je majetkem přírodověděcké fakulty Ostravské univerzity v Ostravě, Česká Republika.',
+                            imageFilePath: 'assets/images/logo_prf.png',
+                          ),
+                          const Divider(),
+                          InfoTile(
+                            screenSize: screenSize,
+                            title: 'Data o místech, osazena Euroklíčem poskytují oficiální stránky Euroklíče, \nkde jsou také volně dostupné veřejnosti.',
+                            imageFilePath: 'assets/images/logo_eurokey.png',
+                          ),
+                        ],
+                      ),
                     ),
-                    const Divider(),
-                    InfoTile(
-                        screenSize: screenSize,
-                        title: 'Data o místech, osazena Euroklíčem poskytují oficiální stránky Euroklíče, \nkde jsou také volně dostupné veřejnosti.',
-                        imageFilePath: 'assets/images/logo_eurokey.png',
-                    ),
-                  ],
+                  ),
                 ),
               ),
             ],
