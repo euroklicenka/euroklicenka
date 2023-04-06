e0d3c25e57fe9092d7c9ff96a7f29adb296f38d4
WaluigiNumber8 <p22157@student.osu.cz>
2023-04-06 07:41:04 +0200

    EUR2-75 Color and image adjustments
    * The color of the dots is now taken from the current theme.
    * All images have rounded borders.
    * Adjusted popup window image size to match with the others.

--- euk2_project/lib/screens/intro_guide_screen.dart
+++ euk2_project/lib/screens/intro_guide_screen.dart
@@ -1,6 +1,4 @@
-
 import 'package:euk2_project/blocs/main_screen_bloc/main_screen_bloc.dart';
-import 'package:euk2_project/screens/main_screen.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:introduction_screen/introduction_screen.dart';
@@ -15,65 +13,69 @@ class GuideScreen extends StatefulWidget {
 class _OnBoardScreenState extends State<GuideScreen> {
   @override
   Widget build(BuildContext context) => SafeArea(
-    child: IntroductionScreen(
-      pages: [
-        PageViewModel(
-            title: 'VÍTEJTE V EUROKLÍČENCE',
-            body: "Díky mobilní aplikace EuroKlíčenka máte možnost najít všechna nejbližší sociální zařízení, která jsou osazena Eurozámkem.",
-            image: buildImage('assets/images/logo_key.png', context),
-            decoration: getDecoration()),
-        PageViewModel(
-            title: 'MAPA',
-            body: "V celé České republice se nachází více jak 1 000 míst, kde se dá použít Euroklíč." ,
-            image: buildImage('assets/images/cr.png', context),
-            decoration: getDecoration()),
-        PageViewModel(
-            title: 'INFORMACE O MÍSTĚ',
-            body: "Po kliknutí na jednu z ikon se zobrazí informační okno s možností navigovat k danému místu." ,
-            image: buildImage('assets/images/maps.png', context),
-            decoration: getDecoration()),
-        PageViewModel(
-          title: 'NEJBLIŽŠÍ MÍSTA',
-          body: "V listu lokací se zobrazí místa, která jsou od aktuální polohy uživatele nejblíže.",
-          image: buildImage('assets/images/list_of_place.png', context),
-          decoration: getDecoration(),
+        child: IntroductionScreen(
+          pages: [
+            PageViewModel(
+                title: 'VÍTEJTE V EUROKLÍČENCE',
+                body: 'Díky mobilní aplikace EuroKlíčenka máte možnost najít všechna nejbližší sociální zařízení, která jsou osazena Eurozámkem.',
+                image: buildImage('assets/images/logo_key.png', context),
+                decoration: getDecoration()),
+            PageViewModel(
+                title: 'MAPA',
+                body: 'V celé České republice se nachází více jak 1 000 míst, kde se dá použít Euroklíč.',
+                image: buildImage('assets/images/cr.png', context),
+                decoration: getDecoration()),
+            PageViewModel(
+                title: 'INFORMACE O MÍSTĚ',
+                body: 'Po kliknutí na jednu z ikon se zobrazí informační okno s možností navigovat k danému místu.',
+                image: buildImage('assets/images/maps.png', context),
+                decoration: getDecoration()),
+            PageViewModel(
+              title: 'NEJBLIŽŠÍ MÍSTA',
+              body: 'V listu lokací se zobrazí místa, která jsou od aktuální polohy uživatele nejblíže.',
+              image: buildImage('assets/images/list_of_place.png', context),
+              decoration: getDecoration(),
+            ),
+          ],
+          animationDuration: 150,
+          done: const Text("Start"),
+          onDone: () => gotoHome(context),
+          showNextButton: true,
+          next: const Icon(Icons.arrow_forward),
+
+          // showing skip button
+          showSkipButton: true,
+          skip: const Text("Skip"),
+          onSkip: () => gotoHome(context),
+          dotsDecorator: getDotDecoration(context),
+          isProgressTap: false,
+
+          nextFlex: 0,
         ),
-      ],
-      done: const Text("Start"),
-      onDone: () => gotoHome(context),
-      showNextButton: true,
-      next: const Icon(Icons.arrow_forward),
-
-      // showing skip button
-      showSkipButton: true,
-      skip: const Text("Skip"),
-      onSkip: () => gotoHome(context),
-      dotsDecorator: getDotDecoration(),
-      onChange: (index) => print("Your in $index Screen."),
-      isProgressTap: false,
-
-      nextFlex: 0,
-    ),
-  );
+      );
 }
 
 void gotoHome(BuildContext context) => context.read<MainScreenBloc>().add(OnInitFinish());
 
 Widget buildImage(String path, BuildContext context) => Center(
-  child: Image.asset(path, width: MediaQuery.of(context).size.width * 0.7 ),
-); // just use arrow function { => }
+      child: ClipRRect(
+        borderRadius: BorderRadius.circular(12),
+        child: Image.asset(path, width: MediaQuery.of(context).size.width * 0.7),
+      ),
+    ); // just use arrow function { => }
 
-DotsDecorator getDotDecoration() => DotsDecorator(
-    //color: Color(0xffbdbdbd),
+DotsDecorator getDotDecoration(BuildContext context) => DotsDecorator(
     size: const Size(10, 10),
     activeSize: const Size(25, 15),
-    activeColor: Colors.amber,
-    activeShape:
-    RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)));
+    activeColor: Theme.of(context).colorScheme.secondary,
+    activeShape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(25)));
 
 PageDecoration getDecoration() => const PageDecoration(
     titleTextStyle: TextStyle(
-        fontSize: 24, color: Colors.black, fontWeight: FontWeight.bold),
+      fontSize: 24,
+      color: Colors.black,
+      fontWeight: FontWeight.bold,
+    ),
     bodyTextStyle: TextStyle(fontSize: 18),
     bodyPadding: EdgeInsets.all(5),
     imagePadding: EdgeInsets.all(15),
