d985188bec51d4a8e331c7732f39f9f27d218669
WaluigiNumber8 <p22157@student.osu.cz>
2023-04-11 13:19:23 +0200

    EUR2-137 Created list of allowed URLs
    * All allowed URLs are now list in a new file allowed_urls.dart
    * Moved http_loader.dart to same directory as allowed_urls.dart
    * LocationManager reads download url from allowed_urls.dart

---
+++ euk2_project/lib/features/internet_access/allowed_urls.dart
@@ -0,0 +1,5 @@
+const String EUKDownloadURL = 'https://www.euroklic.cz/element/simple/documents-to-download/4/0/ccff3b38583129f3.xlsx?download=true';
+const String aboutEuroKeyURL = 'https://www.euroklic.cz/o-cem-jsou-tyto-stranky-a-co-zde-najdete';
+const String aboutEuroKeyWebURL = 'https://portal.gov.cz/rozcestniky/euroklic-RZC-83';
+const String universityOfOstravaURL = 'https://prf.osu.cz/';
+const String universityOfOstravaKIPURL = 'https://prf.osu.cz/kip/';
