2eb24f4bff557eec94973b41e755d7e5c58e1fd7
WaluigiNumber8 <p22157@student.osu.cz>
2023-04-11 13:32:53 +0200

    EUR2-137 Added function for opening URL links
    * Can launch URL links via openURL() in http_loader.dart
    * InfoTile now has a ripple effect when tapped.
    * All InfoTiles on the InformationScreen send the user to the appropriate website.

--- euk2_project/lib/features/internet_access/http_loader.dart
+++ euk2_project/lib/features/internet_access/http_loader.dart
@@ -1,6 +1,8 @@
 import 'dart:io';
 
+import 'package:euk2_project/features/snack_bars/snack_bar_management.dart';
 import 'package:http/http.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 ///Processes the connection between the app and Excel file'S URL address.
 Future<List<int>> getAsBytes({required String url, Function()? onFail}) async {
@@ -13,4 +15,11 @@ Future<List<int>> getAsBytes({required String url, Function()? onFail}) async {
     onFail?.call();
   }
   return [];
+}
+
+///Open the [url] in an external browser.
+Future<void> openURL({required String url}) async {
+  if (!await launchUrl(Uri.parse(url))) {
+    showSnackBar(message: "'$url' nemohla být otevřena.");
+  }
 }
