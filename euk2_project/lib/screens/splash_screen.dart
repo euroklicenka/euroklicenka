12638e1a5aa34042e8b265a84745b6381f449149
WaluigiNumber8 <p22157@student.osu.cz>
2023-04-19 14:45:26 +0200

    EUR2-74 Splash screen supports dark mode
    * The Splash Screen background is now based on current theme.

--- euk2_project/lib/screens/splash_screen.dart
+++ euk2_project/lib/screens/splash_screen.dart
@@ -9,25 +9,22 @@ class EUKSplashScreen extends StatelessWidget {
     final double screenHeight = MediaQuery.of(context).size.height;
 
     return Scaffold(
-      body: ColoredBox(
-        color: Colors.white,
-        child: Center(
-            child: Container(
-              padding: const EdgeInsets.symmetric(vertical: 64, horizontal: 32),
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.end,
-                children: [
-                  Image.asset('assets/images/logo_key.png', height: screenHeight * 0.25,),
-                  const SizedBox(height: 16,),
-                  const Text('EuroKlíčenka', textScaleFactor: 2,),
-                  const SizedBox(height: 86,),
-                  const CircularProgressIndicator(),
-                  SizedBox(height: screenHeight * 0.15,)
-                ],
-              ),
+      body: Center(
+          child: Container(
+            padding: const EdgeInsets.symmetric(vertical: 64, horizontal: 32),
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.end,
+              children: [
+                Image.asset('assets/images/logo_key.png', height: screenHeight * 0.25,),
+                const SizedBox(height: 16,),
+                const Text('EuroKlíčenka', textScaleFactor: 2,),
+                const SizedBox(height: 86,),
+                const CircularProgressIndicator(),
+                SizedBox(height: screenHeight * 0.15)
+              ],
             ),
           ),
-      ),
+        ),
     );
   }
 }
