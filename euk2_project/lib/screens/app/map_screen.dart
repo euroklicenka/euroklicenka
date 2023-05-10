f651858571f4b4250bd60edf29fffdc51fbef600
WaluigiNumber8 <p22157@student.osu.cz>
2023-05-10 19:33:26 +0200

    EUR2-175 Decreased gap height to 16
    * Decreased the indicator-text gap height from 20px to 16px.

--- euk2_project/lib/screens/app/map_screen.dart
+++ euk2_project/lib/screens/app/map_screen.dart
@@ -91,7 +91,7 @@ class _MapScreenState extends State<MapScreen> {
               mainAxisAlignment: MainAxisAlignment.center,
               children: [
                 CircularProgressIndicator(),
-                SizedBox(height: 20,),
+                SizedBox(height: 16,),
                 Text('Vykreslování mapy'),
               ],
             ),
