486be425b906dec4532be968ffac6caccee53547
WaluigiNumber8 <p22157@student.osu.cz>
2023-05-10 17:03:21 +0200

    EUR2-174 Urls are now being read from yaml file
    * All allowed URLs are read from a .yaml file, located in assets.
    * A YAMLDataManager prepares and then sets the urls into allowed_urls.dart

--- euk2_project/lib/features/data_management/yaml_data_manager.dart
+++ euk2_project/lib/features/data_management/yaml_data_manager.dart
@@ -1,10 +1,43 @@
+import 'package:eurokey2/features/internet_access/allowed_urls.dart';
+import 'package:flutter/services.dart';
+import 'package:yaml/yaml.dart';
+
 ///Manages access to custom YAML property files.
 class YAMLDataManager {
-  YAMLDataManager._create();
+  static YAMLDataManager? _instance;
+
+  final String _downloadURLsParam = 'download_urls';
+  final String _aboutEuroKeyUrlParam = 'about_eurokey_url';
+  final String _aboutEuroKeyWebUrlParam = 'about_eurokey_web_url';
+  final String _universityOfOstravaUrlParam = 'university_of_ostrava_url';
+  final String _universityOfOstravaKIPUrlParam = 'university_of_ostrava_kip_url';
+
+  static Future<YAMLDataManager> getInstance() async {
+    _instance ??= await _create();
+    return _instance!;
+  }
+
+  YAMLDataManager._();
+
 
   /// Creates a new instance of [YAMLDataManager].
-  static Future<YAMLDataManager> create() async {
-    final YAMLDataManager m = YAMLDataManager._create();
+  static Future<YAMLDataManager> _create() async {
+    final YAMLDataManager m = YAMLDataManager._();
+    final String rawData = await rootBundle.loadString('assets/url_addresses.yaml');
+    final YamlMap data = await loadYaml(rawData) as YamlMap;
+
+    final List<String> downloads = [];
+    for (final url in data[m._downloadURLsParam] as YamlList) {
+      downloads.add(url.toString());
+    }
+
+    setAllowedURLs(
+        downloads: downloads,
+        aboutEuroKey: data[m._aboutEuroKeyUrlParam].toString(),
+        aboutEuroKeyWeb: data[m._aboutEuroKeyWebUrlParam].toString(),
+        universityOfOstrava:  data[m._universityOfOstravaUrlParam].toString(),
+        universityOfOstravaKIP: data[m._universityOfOstravaKIPUrlParam].toString(),
+    );
 
     return m;
   }
--- euk2_project/lib/features/internet_access/allowed_urls.dart
+++ euk2_project/lib/features/internet_access/allowed_urls.dart
@@ -1,11 +1,16 @@
-//When you add a new URL here, make sure to place it in AndroidManifest.xml
-//and plist.info next to other links as well (so it can be allowed by the device).
 
-List<String> downloadURLs = [
-  'https://www.euroklic.cz/element/simple/documents-to-download/d/0/b828501d0553497b.xlsx?download=true',
-  'https://github.com/ondrej66/RPR1/raw/main/Dokumenty/jiz_osazeno.xlsx'
-];
-const String aboutEuroKeyURL = 'https://portal.gov.cz/rozcestniky/euroklic-RZC-83';
-const String aboutEuroKeyWebURL = 'https://www.euroklic.cz/o-cem-jsou-tyto-stranky-a-co-zde-najdete';
-const String universityOfOstravaURL = 'https://prf.osu.cz/';
-const String universityOfOstravaKIPURL = 'https://prf.osu.cz/kip/';
+
+List<String> downloadURLs = [];
+String aboutEuroKeyURL = '';
+String aboutEuroKeyWebURL = '';
+String universityOfOstravaURL = '';
+String universityOfOstravaKIPURL = '';
+
+/// Sets up all allowed URLs in the file.
+void setAllowedURLs({required List<String> downloads, required String aboutEuroKey, required String aboutEuroKeyWeb, required String universityOfOstrava, required String universityOfOstravaKIP}) {
+  downloadURLs = downloads;
+  aboutEuroKeyURL = aboutEuroKey;
+  aboutEuroKeyWebURL = aboutEuroKeyWeb;
+  universityOfOstravaURL = universityOfOstrava;
+  universityOfOstravaKIPURL = universityOfOstravaKIP;
+}
