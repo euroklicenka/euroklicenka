bb4ab7e0e8f48f9d6e4b89809978c9f371e5b053
WaluigiNumber8 <p22157@student.osu.cz>
2023-05-10 11:45:52 +0200

    EUR2-174 Added yaml package
    * Added the yaml package.
    * Created skeletons for YAMLDataManager and url_adresses.yaml

---
+++ euk2_project/lib/features/data_management/yaml_data_manager.dart
@@ -0,0 +1,11 @@
+///Manages access to custom YAML property files.
+class YAMLDataManager {
+  YAMLDataManager._create();
+
+  /// Creates a new instance of [YAMLDataManager].
+  static Future<YAMLDataManager> create() async {
+    final YAMLDataManager m = YAMLDataManager._create();
+
+    return m;
+  }
+}
