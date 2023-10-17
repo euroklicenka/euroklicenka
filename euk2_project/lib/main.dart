931409c36a70be1365c91f0ddf78e1172b7aa274
Ondřej Surý <ondrej@isc.org>
2023-10-17 16:58:35 +0200

    HF-26: Restore the original sources from develop branch to get started

--- euk2_project/lib/main.dart
+++ euk2_project/lib/main.dart
@@ -11,7 +11,6 @@ import 'package:eurokey2/screens/main_screen.dart';
 import 'package:eurokey2/themes/theme_collection.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
-import 'package:shared_preferences/shared_preferences.dart';
 
 late UserDataManager _dataManager;
 late YAMLDataManager _yamlManager;
