70ed27c58295b694792240ae6bbf8ba959c43f9c
Ondřej Surý <ondrej@isc.org>
2024-01-05 14:30:11 +0100

    HF-17: EasySearch bar has been integrated with Nominatim for address search on the map

--- lib/screens/app/map_screen.dart
+++ lib/screens/app/map_screen.dart
@@ -7,16 +7,27 @@ import 'package:eurokey2/models/preferences_model.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_map/flutter_map.dart';
 import 'package:flutter_map_location_marker/flutter_map_location_marker.dart';
+import 'package:geolocator/geolocator.dart';
 import 'package:provider/provider.dart';
 import 'package:url_launcher/url_launcher.dart';
+import 'package:osm_nominatim/osm_nominatim.dart';
+import 'package:latlong2/latlong.dart';
 
-class MapScreen extends StatelessWidget {
+class MapScreen extends StatefulWidget {
   final MapController mapController = MapController();
 
   MapScreen({super.key});
 
+  @override
+  State<MapScreen> createState() => MapScreenState();
+}
+
+class MapScreenState extends State<MapScreen> {
+  DateTime onSearchlastSearch = DateTime.now();
+  DateTime asyncSuggestionsLastSearch = DateTime.now();
+
   EasySearchBar appBar(BuildContext context) {
-    final locationModel = Provider.of<LocationModel>(context);
+    // final locationModel = Provider.of<LocationModel>(context);
     return EasySearchBar(
       title: const Center(
         child: Text('Mapa nejbližších míst'),
@@ -28,17 +39,107 @@ class MapScreen extends StatelessWidget {
           IconThemeData(color: Theme.of(context).colorScheme.secondary),
       searchCursorColor: Theme.of(context).colorScheme.secondary,
       searchBackgroundColor: Theme.of(context).colorScheme.surface,
-      onSearch: (value) => locationModel.onSearch(value),
+      onSearch: (value) => onSearch(value),
       searchHintText: 'Ostrava...',
+      debounceDuration: const Duration(milliseconds: 1000),
+      asyncSuggestions: asyncSuggestions,
+      // suggestions: lastSuggestions,
+      suggestionBackgroundColor: Theme.of(context).colorScheme.surface,
+      onSuggestionTap: _onSuggestionTap,
     );
   }
+
+  Map<String, Place> lastSuggestions = {};
+
+  String? _onSuggestionTap(String? value) {
+    if (lastSuggestions.containsKey(value)) {
+      final place = lastSuggestions[value]!;
+      widget.mapController.move(LatLng(place.lat, place.lon), 15);
+    }
+
+    return value;
+  }
+
+  Future<List<String>> asyncSuggestions(String? value) async {
+    if (value == null || value.length < 3) {
+      return [];
+    }
+
+    final dt = DateTime.now();
+    final difference = dt.difference(asyncSuggestionsLastSearch);
+    if (difference.inMilliseconds < 1000) {
+      // this should not happen, but just in case...
+      await Future.delayed(
+          Duration(milliseconds: 1000 - difference.inMilliseconds));
+    }
+
+    final searchResult = await Nominatim.searchByName(
+      query: value,
+      limit: 20,
+      addressDetails: true,
+      // extraTags: true,
+      nameDetails: true,
+      // countryCodes: ['cz'],
+    );
+
+    List<String> places = [];
+
+    searchResult.sort((a, b) => Geolocator.distanceBetween(
+            widget.mapController.camera.center.latitude,
+            widget.mapController.camera.center.longitude,
+            a.lat,
+            a.lon)
+        .compareTo(Geolocator.distanceBetween(
+            widget.mapController.camera.center.latitude,
+            widget.mapController.camera.center.longitude,
+            b.lat,
+            b.lon)));
+
+    for (final place in searchResult) {
+      String? town = place.address?['city'] ??
+          place.address?['town'] ??
+          place.address?['village'] ??
+          place.address?['municipality'];
+      String? suburb = place.address?['city_district'] ??
+          place.address?['district'] ??
+          place.address?['borough'] ??
+          place.address?['suburb'] ??
+          place.address?['subdivision'];
+
+      final String? road = place.address?['road'];
+      final String roadStr =
+          (road != null) ? road : suburb ?? "where the streets have no name";
+
+      final houseNumber =
+          place.address?['house_number'] ?? place.address?['house_name'];
+      final String houseNumberStr =
+          (houseNumber != null) ? " $houseNumber" : "";
+
+      final postCode = place.address?['postcode'];
+      final postCodeStr = (postCode != null) ? "$postCode " : "";
+
+      final addressStr = "$roadStr$houseNumberStr, $postCodeStr$town";
+
+      print(place.nameDetails);
+
+      lastSuggestions[addressStr] = place;
+
+      // FIXME: sort by distance???
+      places.add(addressStr);
+    }
+
+    asyncSuggestionsLastSearch = DateTime.now();
+    return places;
+  }
+
+  Future<void> onSearch(String value) async {}
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       resizeToAvoidBottomInset: false,
       appBar: appBar(context),
-      body: MapScreenBody(mapController: mapController),
+      body: MapScreenBody(mapController: widget.mapController),
     );
   }
 }
@@ -86,6 +187,10 @@ class _MapScreenState extends State<MapScreenBody> {
     }
 
     final locationProvider = Provider.of<LocationModel>(context, listen: false);
+    if (_followOnLocationUpdate != AlignOnUpdate.never) {
+      locationProvider.currentUserPosition = mapPosition.center!;
+    }
+
     locationProvider.currentMapPosition = mapPosition.center!;
     locationProvider.currentMapZoom = mapPosition.zoom!;
   }
